00990772b8a152ed3f62d3539e5d1d3395b98c63
Segoul01 <129515380+Segoul01@users.noreply.github.com>
2025-10-28 18:59:12 +0000

    Added GetLetterFromHouseID(int houseID)

--- Assets/Scripts/Player/PlayerInventoryManager.cs
+++ Assets/Scripts/Player/PlayerInventoryManager.cs
@@ -9,6 +9,16 @@ public class PlayerInventoryManager : MonoBehaviour
 
     public LetterData GetLetter(int letterIndex) => letters[letterIndex];
 
+    public LetterData GetLetterFromHouseID(int houseID)
+    {
+        foreach (LetterData letter in letters)
+        {
+            if (letter.houseID == houseID)
+                return letter;
+        }
+        return null;
+    }
+
     public void AddLetter(LetterData newLetter) => letters.Add(newLetter);
 
     public void RemoveLetter(LetterData letter) => letters.Remove(letter);
