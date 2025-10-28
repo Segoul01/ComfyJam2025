8de9a1838f31b661d665afb94931842c739a6d0c
Segoul01 <129515380+Segoul01@users.noreply.github.com>
2025-10-28 18:54:50 +0000

    Created Letter Data and Player Inventory

---
+++ Assets/Scripts/Player/PlayerInventoryManager.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventoryManager : MonoBehaviour
+{
+    private List<LetterData> letters = new List<LetterData>();
+
+    public List<LetterData> GetLetters() => letters;
+
+    public LetterData GetLetter(int letterIndex) => letters[letterIndex];
+
+    public void AddLetter(LetterData newLetter) => letters.Add(newLetter);
+
+    public void RemoveLetter(LetterData letter) => letters.Remove(letter);
+}
---
+++ Assets/Scripts/ScriptableObjects/LetterData.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LetterData", menuName = "Scriptable Objects/LetterData")]
+public class LetterData : ScriptableObject
+{
+    public int houseID;
+    public string title;
+    public string sender;
+    public string receiver;
+    
+    [TextArea(15,20)]
+    public string content;
+}
