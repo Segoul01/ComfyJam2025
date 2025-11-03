34220fc7bfda66772fb27dd1700d09aae493ea38
Segoul01 <129515380+Segoul01@users.noreply.github.com>
2025-11-03 16:01:57 +0000

    Added Parallax

---
+++ Assets/Scripts/Camera/ParallaxEffect.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ParallaxEffect : MonoBehaviour
+{
+    private float length, startPos;
+    [SerializeField] private GameObject cam;
+    [SerializeField] private float parallaxStrength;
+
+    float dist;
+    float temp;
+
+    private void Start()
+    {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void FixedUpdate()
+    {
+        temp = cam.transform.position.x * (1 - parallaxStrength);
+        dist = cam.transform.position.x * parallaxStrength;
+
+        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startPos + length) startPos += length;
+        else if (temp < startPos - length) startPos -= length;
+    }
+}
